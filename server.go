1f6e17d3683c98aedff63f65290fa73136ca6b57
pchchv <ipchchv@gmail.com>
2022-12-25 20:23:20 +0300

    Implement a loading values from .env

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// Load values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value
+	// Outputs a panic if the value is missing
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("Value %v does not exist", v)
+	}
+	return value
+}
+
+func main() {
+	port := getEnvValue("PORT")
+
+}
