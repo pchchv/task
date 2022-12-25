2706496d27dacccf03f88a8a5e65a37a1418c606
pchchv <ipchchv@gmail.com>
2022-12-25 20:18:00 +0300

    Rfactoring to models package

---
+++ graph/model/models.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type Attachment struct {
+	Name     string    `json:"Name"`
+	Date     time.Time `json:"Date"`
+	Contents string    `json:"Contents"`
+}
+
+type Task struct {
+	ID          int           `json:"Id"`
+	Text        string        `json:"Text"`
+	Tags        []string      `json:"Tags"`
+	Due         time.Time     `json:"Due"`
+	Attachments []*Attachment `json:"Attachments"`
+}
--- main.go
+++ main.go
@@ -1,17 +1,1 @@
 package main
-
-import "time"
-
-type Attachment struct {
-	Name     string    `json:"Name"`
-	Date     time.Time `json:"Date"`
-	Contents string    `json:"Contents"`
-}
-
-type Task struct {
-	ID          int           `json:"Id"`
-	Text        string        `json:"Text"`
-	Tags        []string      `json:"Tags"`
-	Due         time.Time     `json:"Due"`
-	Attachments []*Attachment `json:"Attachments"`
-}
